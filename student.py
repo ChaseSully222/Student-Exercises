7a1ad6cf21f8dd297f3f8b68646778e33c157473
ChaseSully222 <Jcsullins222@yahoo.com>
2020-05-01 10:48:33 -0500

    completed ch15 student exercise 2

--- student.py
+++ student.py
@@ -1,3 +1,5 @@
+from nssperson import NSSPerson
+
 # You must define a custom type for representing a student in code. A student can only be in one cohort at a time. A student can be working on many exercises at a time.
 
 # Properties
@@ -8,13 +10,10 @@
 # The collection of exercises that the student is currently working on
 
 
-class Student:
+class Student(NSSPerson):
 
     def __init__(self, firstName, lastName, slack):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.slack = slack
-        self.cohort = ""
+        super().__init__(firstName, lastName, slack)
         self.exercises = list()
 
     def student_summary(self):
